6c563cd2d8d91b36d5e8b95418062729692f768c
William Leuschner <wel2138@rit.edu>
2017-02-07 11:56:12 -0500

    Start the project

---
+++ src/headcount.py
@@ -0,0 +1,6 @@
+# Headcount.py
+# A Server-Side Python Application for Labbies Taking Headcounts in SE Classrooms
+# Author: William Leuschner
+# File Creation Date: 2017-02-01
+# Last Modified Date: 2017-02-01
+
