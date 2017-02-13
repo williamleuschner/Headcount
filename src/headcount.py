da95e4a30f0dacfb9f58cd1f57f95f63584117dc
William Leuschner <wel2138@rit.edu>
2017-02-13 10:51:01 -0800

    Flask!

--- src/headcount.py
+++ src/headcount.py
@@ -4,3 +4,10 @@
 # File Creation Date: 2017-02-01
 # Last Modified Date: 2017-02-01
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return("Hello, world!")
