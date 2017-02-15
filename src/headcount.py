19a936bfaaa4f68b9df460f741e9bc642fb187d5
William Leuschner <wel2138@rit.edu>
2017-02-15 06:50:30 -0800

    More skeleton code

--- src/headcount.py
+++ src/headcount.py
@@ -13,6 +13,22 @@ app = Flask(__name__)
 def hello_world():
     return("Hello, world!")
 
+@app.route("/main")
+def show_main():
+    return("This is the main count entry screen")
+
+@app.route("/admin")
+def show_admin():
+    return("This is the administration page")
+
+@app.route("/admin/edit-admins")
+def show_admin_edit_admins():
+    return("This is the administration page, but in edit mode for administrators")
+
+@app.route("/admin/edit-users")
+def show_admin_edit_users():
+    return("This is the administration page, but in edit mode for users")
+
 
 def main():
     config = config_lexer.read_configuration()
